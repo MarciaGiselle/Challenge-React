6cabfb2388538c826a1da9abcd264bfc4009fed9
MarciaGiselle <margisetoledo@gmail.com>
2022-09-23 21:42:53 -0300

    Agregando Material Design y base de elementos

--- challenge-books/src/App.js
+++ challenge-books/src/App.js
@@ -1,7 +1,42 @@
 
+import Sidebar from './Components/Sidebar';
+import './index.css'
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/Grid';
+
 function App() {
   return (
-    <h2>Hello world</h2>
+      <div>
+      <div className='side-top'></div>
+
+      <div className="contenedor-app">
+      <div className='side-left'></div>
+      
+      <Sidebar />
+      <div className="seccion-principal">
+          <main>
+          <section className="section">
+            <section className="section-item">hola</section>
+            <section className="section-item">hola dos</section>
+            <section className="section-item">hola tres</section>
+            <section className="section-item">hola cuatrossssssssssssss</section>
+          </section>
+
+          <section className="section">
+            <section className="section-item">
+              
+              <h1>hellou</h1></section>
+            </section>
+
+            <section className="section">
+            <section className="section-item">hola</section>
+            </section>
+          </main>
+        
+      </div>
+      </div>
+
+      </div>
   );
 }
 
