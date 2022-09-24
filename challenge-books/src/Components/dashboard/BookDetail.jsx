5ed21f50d8f5b42dae740dd9e583b790376cb151
MarciaGiselle <margisetoledo@gmail.com>
2022-09-24 20:57:33 -0300

    Agregando componentes Detalle

---
+++ challenge-books/src/Components/dashboard/BookDetail.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const BookDetail = () => {
+    return ( 
+        <div className='flex-container'>
+            <div className="section-item mr-1 text-center">
+                <img className='card-image-rounded' src="https://www.energygreenmap.org/logo_greenmap.svg" alt="LOGO GREENMAP"/>
+                <p><b>Twitter</b></p>
+            </div>
+            <div className="section-item-two mr-1 ">
+                <h4 className='text-cut'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate consequuntur perferendis odio itaque! Cupiditate, quas. </h4>
+                <p className='text-cut '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate consequuntur perferendis odio itaque! Cupiditate, quas. </p>
+
+                <div className='flex-container '>
+                    <div className="section-item mr-1">
+                       <span> <i class='far fa-comment'></i>
+                        200</span>
+                    </div>
+                    <div className="section-item mr-1">
+                        <span> <i class="material-icons"></i>
+                        9:00 PM</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+     );
+}
+ 
+export default BookDetail;
--- challenge-books/src/Components/dashboard/SectionRightThree.jsx
+++ challenge-books/src/Components/dashboard/SectionRightThree.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import BookDetail from './BookDetail';
 
 const SectionRightThree = () => {
     return ( 
-        <div className="card-light height-130">
+        <div className="card-light height-130 pt-2">
         <div className='flex-container'>
-           <div className="section-item mr-1">1</div>
-           <div className="section-item ">2</div>
+           <div className="section-item mr-1 pr-2">
+                <BookDetail/>
+           </div>
+           <div className="section-item pr-2">
+             <BookDetail/>
+           </div>
           </div>
       </div>
      );
