22f3041d15bcc798b5a24905d383435665eb2773
MarciaGiselle <margisetoledo@gmail.com>
2022-09-24 18:12:01 -0300

    Mejorando el listado

--- challenge-books/src/Components/dashboard/Book.jsx
+++ challenge-books/src/Components/dashboard/Book.jsx
@@ -4,20 +4,21 @@ import React from 'react'
 const Book = ({book}) => {
     return (  
         <div  className='flex-container-list'>  
-            <div className="section-item  mr-1">
+            <div className="section-item-two  mr-1">
              <h4>{book.volumeInfo.title}</h4>
             </div>
             <div className="section-item  mr-1">
              <p>{book.volumeInfo.publishedDate}</p>
             </div>
-            <div className="section-item  mr-1">
+            <div className="section-item-two  mr-1">
              <p>{book.volumeInfo.authors}</p>
             </div>
+            
             <div className="section-item  mr-1">
-             <p>{book.volumeInfo.language}</p>
-            </div>
-            <div className="section-item  mr-1">
-             <p>{book.volumeInfo.infoLink}</p>
+            <a href={book.volumeInfo.infoLink} target="_blank">
+                <i className="fas fa-eye"></i>
+                 Ver más
+                </a>
             </div>
         </div>
           
--- challenge-books/src/Components/dashboard/SectionRightOne.jsx
+++ challenge-books/src/Components/dashboard/SectionRightOne.jsx
@@ -4,7 +4,11 @@ const SectionRightOne = () => {
     return ( 
         <div className='height-110 mb-1'>
                 <div className='flex-container'>
-                  <div className="section-item card-light mr-1">1</div>
+                  <div className="section-item card-light mr-1">
+                  <a href="https://www.w3schools.com">
+                    <img border="0" alt="W3Schools" src="logo_w3s.gif" width="100" height="100"></img>
+                    </a>
+                  </div>
                   <div className="section-item card-light mr-1">2</div>
                   <div className="section-item card-light mr-1">3</div>
                   <div className="section-item card-light"> 4</div>
