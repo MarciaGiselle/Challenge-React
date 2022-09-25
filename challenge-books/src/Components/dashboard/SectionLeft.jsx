ed5b139cad494c10c6d5f58035ceb91d1eca3c4e
MarciaGiselle <margisetoledo@gmail.com>
2022-09-24 21:46:56 -0300

    Agregando elementos de la barra lateral

--- challenge-books/src/Components/dashboard/SectionLeft.jsx
+++ challenge-books/src/Components/dashboard/SectionLeft.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 const SectionLeft = () => {
     return (
-        <div className='section-left'>
-               
-             </div>
+        <div className='section-left  '>
+          <div className='justify-end'>
+            <div className='green-rounded'>
+              1
+            </div>       
+            <div className='green-rounded'>
+              2
+            </div>
+            <div className='green-rounded'>
+              3
+            </div>
+            <div className='green-rounded'>
+              4
+            </div>
+         </div>
+        </div>
       );
 }
  
